8065e267e3ff086c36fce2b0fb5beec8c9331a3e
oguzhan gurbuz <grboguz@gmail.com>
2026-01-13 04:29:29 +0000

    adjust filename

---
+++ tortoisebot_waypoints/test/tortoisebot_waypoint_cases.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import rospy
+import rosunit
+import unittest
+import rostest
+from geometry_msgs.msg import Twist
+import time
+
+PKG = 'tortoisebot_waypoints'
+NAME = 'tortoisebot_waypoint_cases'
+
+
+class CaseA(unittest.TestCase):
+
+    def setUp(self):
+        self.rc = RobotControl()
+
+    def runTest(self):
+        self.assert_(True)
+
+class CaseB(unittest.TestCase):
+
+    def setUp(self):
+        self.rc = RobotControl()
+
+    def runTest(self):
+        self.assert_(True)
+class MyTestSuite(unittest.TestSuite):
+
+    def __init__(self):
+        super(MyTestSuite, self).__init__()
+        self.addTest(CaseA())
+        self.addTest(CaseB())
